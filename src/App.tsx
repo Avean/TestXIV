10607e99e29a5db24a853c37ab8fe46c220a0b44
Symon Cygan <Szymon.Cygan.FS@gmail.com>
2026-03-23 17:28:48 +0100

    Change Router from BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import { motion, AnimatePresence } from 'motion/react';
 import { 
   Calendar, MapPin, Mail, ExternalLink, Menu, X, 
